748c7dc95309d6d20dee8bfc4ae2153d0ed50812
manishj <manishj@microsoft.com>
2026-01-07 15:46:35 -0800

    updated the UI and fixed bugs found in testing

--- src/Codenames.Core/Domain/Generation/BoardGenerator.cs
+++ src/Codenames.Core/Domain/Generation/BoardGenerator.cs
@@ -19,8 +19,8 @@ public sealed class BoardGenerator
         _wordProvider = wordProvider ?? throw new ArgumentNullException(nameof(wordProvider));
     }
 
-    private const int RedCount = 9;
-    private const int BlueCount = 8;
+    private const int StartingTeamCount = 9;
+    private const int OtherTeamCount = 8;
     private const int NeutralCount = 7;
     private const int AssassinCount = 1;
 
@@ -30,13 +30,13 @@ public sealed class BoardGenerator
 
         var words = _wordProvider.GetWords(GameBoard.Size, rng);
 
-        // assign types list with distribution then shuffle positions
-        var redAgents = startingTeam == Team.Red ? RedCount : BlueCount;
-        var blueAgents = startingTeam == Team.Red ? BlueCount : RedCount; // inverse if Blue starts
+        // Starting team gets 9 agents, other team gets 8
+        var redAgents = startingTeam == Team.Red ? StartingTeamCount : OtherTeamCount;
+        var blueAgents = startingTeam == Team.Blue ? StartingTeamCount : OtherTeamCount;
 
         var types = new List<CardType>(GameBoard.Size);
-        types.AddRange(Enumerable.Repeat(startingTeam == Team.Red ? CardType.RedAgent : CardType.BlueAgent, redAgents));
-        types.AddRange(Enumerable.Repeat(startingTeam == Team.Red ? CardType.BlueAgent : CardType.RedAgent, blueAgents));
+        types.AddRange(Enumerable.Repeat(CardType.RedAgent, redAgents));
+        types.AddRange(Enumerable.Repeat(CardType.BlueAgent, blueAgents));
         types.AddRange(Enumerable.Repeat(CardType.Neutral, NeutralCount));
         types.Add(CardType.Assassin);
 
